88b64b3519db0848257e8fc909fd5f779168c190
Vsevolod Zubarev <yandexx@gmail.com>
2018-02-05 23:13:21 +0300

    Портировал демку RuinsR.inf

--- demos/demos.bat
+++ demos/demos.bat
@@ -2,7 +2,7 @@
 ..\inform.exe +..\library +language_name=Russian -DG -Cu $DICT_CHAR_SIZE=4 heidi.inf Heidi.ulx
 ..\inform.exe +..\library +language_name=Russian -DG -Cu $DICT_CHAR_SIZE=4 AliceR.inf AliceR.ulx
 ..\inform.exe +..\library +language_name=Russian -DG -Cu $DICT_CHAR_SIZE=4 CloakR.inf CloakR.ulx
-rem ..\inform-633.exe +..\library,..\libext +language_name=Russian -DG -Cu $DICT_CHAR_SIZE=4 RuinsR.inf RuinsR.ulx
+..\inform.exe +..\library,..\libext +language_name=Russian -DG -Cu $DICT_CHAR_SIZE=4 RuinsR.inf RuinsR.ulx
 ..\inform.exe +..\library +language_name=Russian -DG -Cu $DICT_CHAR_SIZE=4 WTellR.inf WTellR.ulx
 
 pause
