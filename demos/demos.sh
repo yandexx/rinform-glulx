9c635bc4ba901453f172fd0e62e53e8f439af6c0
Vsevolod Zubarev <yandexx@gmail.com>
2018-02-06 10:55:05 +0300

    Вернул, протестировал demos.sh для сборки на *nix

---
+++ demos/demos.sh
@@ -0,0 +1,7 @@
+inform +../library,../libext +language_name=Russian -DG -Cu \$DICT_CHAR_SIZE=4 AdventR.inf AdventR.ulx
+inform +../library +language_name=Russian -DG -Cu \$DICT_CHAR_SIZE=4 Heidi.inf Heidi.ulx
+inform +../library +language_name=Russian -DG -Cu \$DICT_CHAR_SIZE=4 AliceR.inf AliceR.ulx
+inform +../library +language_name=Russian -DG -Cu \$DICT_CHAR_SIZE=4 CloakR.inf CloakR.ulx
+inform +../library,../libext +language_name=Russian -DG -Cu \$DICT_CHAR_SIZE=4 RuinsR.inf RuinsR.ulx
+inform +../library +language_name=Russian -DG -Cu \$DICT_CHAR_SIZE=4 WTellR.inf WTellR.ulx
+
