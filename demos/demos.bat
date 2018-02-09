b1adc63b1f38bc79a0cc5207c0e764d96471913e
Vsevolod Zubarev <yandexx@gmail.com>
2018-02-09 11:54:54 +0300

    Порт демки HintsDemo.inf и расширения HelpRoutines.h

--- demos/demos.bat
+++ demos/demos.bat
@@ -4,5 +4,5 @@
 ..\inform.exe +..\library +language_name=Russian -DG -Cu $DICT_CHAR_SIZE=4 CloakR.inf CloakR.ulx
 ..\inform.exe +..\library,..\libext +language_name=Russian -DG -Cu $DICT_CHAR_SIZE=4 RuinsR.inf RuinsR.ulx
 ..\inform.exe +..\library +language_name=Russian -DG -Cu $DICT_CHAR_SIZE=4 WTellR.inf WTellR.ulx
-
+..\inform.exe +..\library,..\libext +language_name=Russian -DG -Cu $DICT_CHAR_SIZE=4 $MAX_UNICODE_CHARS=128 HintsDemo.inf HintsDemo.ulx
 pause
--- demos/demos.sh
+++ demos/demos.sh
@@ -4,4 +4,4 @@ inform +../library +language_name=Russian -DG -Cu \$DICT_CHAR_SIZE=4 AliceR.inf 
 inform +../library +language_name=Russian -DG -Cu \$DICT_CHAR_SIZE=4 CloakR.inf CloakR.ulx
 inform +../library,../libext +language_name=Russian -DG -Cu \$DICT_CHAR_SIZE=4 RuinsR.inf RuinsR.ulx
 inform +../library +language_name=Russian -DG -Cu \$DICT_CHAR_SIZE=4 WTellR.inf WTellR.ulx
-
+inform +../library,../libext +language_name=Russian -DG -Cu \$DICT_CHAR_SIZE=4 \$MAX_UNICODE_CHARS=128 HintsDemo.inf HintsDemo.ulx
