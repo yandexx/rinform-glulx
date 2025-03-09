422ba618f5d19567299b9108c09369be3da3d53a
Nikita Tseykovets <tseikovets@rambler.ru>
2025-03-09 22:30:10 +0300

    Replace the long license notice at the beginning of source files with short one with SPDX License Identifier

--- library/RussiaG.h
+++ library/RussiaG.h
@@ -8,20 +8,7 @@
 !
 !   Copyright (C) 2002 Grankin Andrey
 !   Copyright (C) 2003, 2004 Gayev Denis
-!
-!   This library is free software; you can redistribute it and/or
-!   modify it under the terms of the GNU Library General Public
-!   License as published by the Free Software Foundation version 2.
-!
-!   This library is distributed in the hope that it will be useful,
-!   but WITHOUT ANY WARRANTY; without even the implied warranty of
-!   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
-!   Library General Public License for more details.
-!
-!   You should have received a copy of the GNU Library General Public
-!   License along with this library; if not, write to the
-!   Free Software Foundation, Inc., 51 Franklin St, Fifth Floor,
-!   Boston, MA  02110-1301, USA.
+!   SPDX-License-Identifier: LGPL-2.0-only
 !
 !  Based on: English grammar Release 6/11 Serial number 040227
 !
